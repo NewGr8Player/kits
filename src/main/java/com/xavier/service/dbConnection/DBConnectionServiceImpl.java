9eaa1fb03871755bc8edcc925a9442946f557a57
NewGr8Player <273221594@qq.com>
2018-01-19 23:46:31 +0800

    增加日志功能

--- src/main/java/com/xavier/service/dbConnection/DBConnectionServiceImpl.java
+++ src/main/java/com/xavier/service/dbConnection/DBConnectionServiceImpl.java
@@ -1,13 +1,17 @@
 package com.xavier.service.dbConnection;
 
 import com.xavier.bean.dbConnection.DBConnection;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DBConnectionServiceImpl implements DBConnectionService {
 
+	private static final Log LOGGER = LogFactory.getLog(DBConnectionServiceImpl.class);
+
 	@Override
 	public void findConnectionDetails(DBConnection dbConnection) {
-		System.out.println(dbConnection);
+		LOGGER.info(dbConnection);
 	}
 }
